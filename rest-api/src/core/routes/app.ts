dc0add9ada0436d0e97e50801506357f2343e112
Larry9194 <107245540+Larry9194@users.noreply.github.com>
2024-11-18 13:47:01 +0100

    rename events.routes

--- rest-api/src/core/routes/app.ts
+++ rest-api/src/core/routes/app.ts
@@ -1,7 +1,8 @@
 import { globalErrorHandler, notFoundErrorHandler } from '@core/middlewares/errors';
 import auth from '@features/auth/routes/auth.routes';
-import events from '@features/places/routes/events.routers';
+import events from '@features/places/routes/events.routes';
 import spots from '@features/places/routes/spots.routes';
+import circle from '@features/places/routes/circle.routes';
 import swaggerOptions from '@options/swagger.options';
 import cookies from 'cookie-parser';
 import express from 'express';
@@ -37,6 +38,7 @@ app.use('/v1/auth', auth);
 
 app.use('/v1/events', events);
 app.use('/v1/spots', spots);
+app.use('/v1/circle', circle);
 
 // Handle unregistered route for all HTTP Methods
 app.all('*', notFoundErrorHandler);
