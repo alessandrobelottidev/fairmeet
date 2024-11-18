3be22e6bcebd23952e2175251d8ce461e991b53a
MarcoAdami <smarco.adami@gmail.com>
2024-11-18 13:51:30 +0100

    fix app import

--- rest-api/src/core/routes/app.ts
+++ rest-api/src/core/routes/app.ts
@@ -2,7 +2,6 @@ import { globalErrorHandler, notFoundErrorHandler } from '@core/middlewares/erro
 import auth from '@features/auth/routes/auth.routes';
 import events from '@features/places/routes/events.routes';
 import spots from '@features/places/routes/spots.routes';
-import circle from '@features/places/routes/circle.routes';
 import swaggerOptions from '@options/swagger.options';
 import cookies from 'cookie-parser';
 import express from 'express';
@@ -38,7 +37,6 @@ app.use('/v1/auth', auth);
 
 app.use('/v1/events', events);
 app.use('/v1/spots', spots);
-app.use('/v1/circle', circle);
 
 // Handle unregistered route for all HTTP Methods
 app.all('*', notFoundErrorHandler);
