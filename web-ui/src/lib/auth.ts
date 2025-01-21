90c87ed85eb039558df83a2e718f2d0ba08e5919
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2025-01-22 00:25:41 +0100

    Fixed missing refresh token cookie in logout action

--- web-ui/src/lib/auth.ts
+++ web-ui/src/lib/auth.ts
@@ -123,12 +123,14 @@ export async function logout() {
   const cookieStore = cookies();
   const accessToken = (await cookieStore).get("accessToken")?.value;
   const origin = (await headers()).get("origin");
+  const cookieHeader = (await headers()).get("cookie");
 
   const response = await fetch(`${API_URL}/v1/auth/logout`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${accessToken}`,
       Origin: origin || "",
+      Cookie: cookieHeader || "",
     },
     credentials: "include",
   });
@@ -149,12 +151,14 @@ export async function logoutControlPanel() {
   const cookieStore = cookies();
   const accessToken = (await cookieStore).get("accessToken")?.value;
   const origin = (await headers()).get("origin");
+  const cookieHeader = (await headers()).get("cookie");
 
   const response = await fetch(`${API_URL}/v1/auth/logout`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${accessToken}`,
       Origin: origin || "",
+      Cookie: cookieHeader || "",
     },
     credentials: "include",
   });
