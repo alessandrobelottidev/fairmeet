50452e2f5c23dd284b9c284f3d967c06768d320a
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2025-02-05 00:55:34 +0100

    place item component

--- web-ui/next.config.ts
+++ web-ui/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "none.com"], // Should figure out a better way for this. I do not want to host files though.
+  },
 };
 
 export default nextConfig;
