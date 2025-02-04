28a98388ffe61af207df258e05cf74ee5e0f4f11
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2025-02-04 14:22:44 +0100

    clear out non necessary comments and wrong html elements

--- web-ui/src/app/(protected)/(home)/chat/page.tsx
+++ web-ui/src/app/(protected)/(home)/chat/page.tsx
@@ -1,12 +1,3 @@
-import ActionBar from "@/components/actionbar/Actionbar";
-import Link from "next/link";
-
 export default function Chat() {
-  return (
-    <>
-      <Link href="/organize-meetup/step-1">
-        <ActionBar name="Organizza ritrovo" />
-      </Link>
-    </>
-  );
+  return <></>;
 }
--- web-ui/src/components/map/Map.tsx
+++ web-ui/src/components/map/Map.tsx
@@ -1,8 +1,5 @@
-// pages/Mappa.js
 "use client";
 
-import ActionBar from "@/components/actionbar/Actionbar";
-import Link from "next/link";
 import { useEffect } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -28,26 +25,6 @@ export default function Map({
       shadowSize: [41, 41],
     });
 
-    // Green marker variant
-    const greenIcon = L.icon({
-      iconUrl:
-        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41],
-    });
-
-    // Circle marker (no image needed)
-    const circleMarkerOptions = {
-      radius: 8,
-      fillColor: "#ff7800",
-      color: "#000",
-      weight: 1,
-      opacity: 1,
-      fillOpacity: 0.8,
-    };
-
     map = L.map("map").setView([latitude, longitude], 15);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -59,28 +36,10 @@ export default function Map({
       .addTo(map)
       .bindPopup("Red Marker");
 
-    // L.marker([51.51, -0.09], { icon: greenIcon })
-    //   .addTo(map)
-    //   .bindPopup("Green Marker");
-
-    // L.circleMarker([51.515, -0.1], circleMarkerOptions)
-    //   .addTo(map)
-    //   .bindPopup("Circle Marker");
-
     return () => {
       map.remove();
     };
   }, []);
-
-  // useEffect(() => {
-  //   const logView = () => {
-  //     const center = map.getCenter();
-  //     latitude = center.lat;
-  //     longitude = center.lng;
-  //   };
-
-  //   map.on("moveend", logView);
-  // }, [map]);
 
   return (
     <div
@@ -96,7 +55,4 @@ export default function Map({
       }}
     />
   );
-}
-function useMap() {
-  throw new Error("Function not implemented.");
 }
