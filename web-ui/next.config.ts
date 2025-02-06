1c4859d5deffa5f8d1c97ea0790b592b5676d816
Alessandro Belotti <alessandro.belotti2004@gmail.com>
2025-02-06 01:25:28 +0100

    redirect / to /map

--- web-ui/next.config.ts
+++ web-ui/next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com", "none.com"], // Should figure out a better way for this. I do not want to host files though.
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/map",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
